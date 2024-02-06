4a34ec0886bc1c1103b948e8fc53f1137f542b80
NagaMaruthi <nagamaruthi567@gmail.com>
2024-02-06 07:36:57 +0530

    submit,save,edit,delete

--- newapp1/src/todo.jsx
+++ newapp1/src/todo.jsx
@@ -1,40 +1,69 @@
-import React from "react";
-function Todo(){
-    var [list,setList]=React.useState([
+
+import React, { useState } from "react";
+
+function Todo() {
+    const [list, setList] = useState([
         "maruthi",
         "naga",
         "gopi",
         "vamsi",
         "govind",
     ]);
-    console.log(list);
-   
-    function abc(){
-        var ip=document.getElementById("kl").value;
-        alert(ip);
-        setList([...list,ip])
+
+    const [editIndex, setEditIndex] = useState(null);
+    const [editValue, setEditValue] = useState("");
+
+    const handleAdd = () => {
+        const inputValue = document.getElementById("kl").value;
+        setList([...list, inputValue]);
     };
-    function bca(i){
-     list.splice(i,1),
-     setList([...list])  
-    }
-    function op(i){
-        
-    }
-    return(
+
+    const handleDelete = (index) => {
+        const newList = [...list];
+        newList.splice(index, 1);
+        setList(newList);
+    };
+
+    const handleEdit = (index) => {
+        setEditIndex(index);
+        setEditValue(list[index]);
+    };
+
+    const handleSaveEdit = () => {
+        const newList = [...list];
+        newList[editIndex] = editValue;
+        setList(newList);
+        setEditIndex(null);
+        setEditValue("");
+    };
+
+    return (
         <div>
             <h1>Todo List</h1>
-            <input  placeholder="Enter any name" id="kl"></input>
-            <button onClick={abc} >submit</button>
-            {
-                list.map((lists,i)=>{
-                    return(<li>{lists}
-                    <button onClick={()=>{bca(i)}}>deleted</button>
-                    <button onClick={()=>{op(i)}}>Edit</button></li>
-                    )
-                })
-            }
+            <input placeholder="Enter any name" id="kl"></input>
+            <button onClick={handleAdd}>Submit</button>
+            {list.map((item, index) => (
+                <li key={index}>
+                    {editIndex === index ? (
+                        <>
+                            <input
+                                type="text"
+                                value={editValue}
+                                onChange={(e) => setEditValue(e.target.value)}
+                            />
+                            <button onClick={handleSaveEdit}>Save</button>
+                        </>
+                    ) : (
+                        <>
+                            {item}
+                            <button onClick={() => handleDelete(index)}>Delete</button>
+                            <button onClick={() => handleEdit(index)}>Edit</button>
+                        </>
+                    )}
+                </li>
+            ))}
         </div>
-    )
+    );
 }
+
 export default Todo;
